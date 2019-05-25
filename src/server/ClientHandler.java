2254fcf0392d143a4a26b850737dc4c10205b271
ligovean <ligomail@rambler.ru>
2019-05-25 16:16:09 +0300

    Client Server

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,71 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientHandler {
+    private String clientName;
+    private Socket socket;
+    private ServerMain server;
+    private DataInputStream input;
+    private DataOutputStream output;
+
+    public ClientHandler(Socket socket, ServerMain server, String name) {
+            this.socket = socket;
+            this.server = server;
+            this.clientName = name;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    input = new DataInputStream(socket.getInputStream());
+                    output = new DataOutputStream(socket.getOutputStream());
+
+                    server.broadcastMsg("======/" + name + " вошел в чат!/======");
+
+                    while (true) {
+                        String msg = input.readUTF();
+                        System.out.println(msg);
+                        if(msg.equals("/end")) {
+                            System.out.println("Сокет " + socket.getRemoteSocketAddress() + " закрыт!");
+                            socket.close();
+                            break;
+                        }
+                        //output.writeUTF("Client " + socket.getRemoteSocketAddress() + ": " + msg);
+                        server.broadcastMsg(name + ": " + msg);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            output.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
