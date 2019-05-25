79e6ca1e45a2af0c910674f908a516660ae2db2a
ligovean <ligomail@rambler.ru>
2019-05-25 14:29:24 +0300

    Client

--- src/client/sample/Main.java
+++ src/client/sample/Main.java
@@ -4,15 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Chat");
         primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setMaxWidth(300);
+        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(275);
+        primaryStage.getIcons().add(new Image("/client/css/img/btnArr1.png"));
         primaryStage.show();
     }
 
